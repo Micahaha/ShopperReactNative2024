c41f5c9b7367836546562045b1b05b9e5bf29be3
Micahaha <xicer029@gmail.com>
2024-01-31 00:22:39 -0500

    Tuesday

--- src/screens/Lists/Index.js
+++ src/screens/Lists/Index.js
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './styles';
 import {View, Text, TextInput, Pressable } from 'react-native'
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import { TouchableOpacity, FlatList } from 'react-native-gesture-handler';
 import { useNavigation } from '@react-navigation/native';
+import List from '../../components/List';
+import { openDatabase } from "react-native-sqlite-storage";
+ 
 
 const ListsScreen = props => {
 
   const navigation = useNavigation()
 
+  const [lists, setLists] = useState([]);
+
+  useEffect(() => {
+    const listener = navigation.addListener('focus', () => {
+      // declare empty array that will store results of SELECT
+      let results = [];
+      // declare transaction that will execute SELECT
+      shopperDB.transaction(txn => {
+
+      })
+
+    })
+  })
+
   return (
     <View style={styles.container}>
+      <View>
+        <FlatList
+        data={lists}
+        renderItem={({item}) => <List post={item} />}
+        />
+      </View>
         <View style={styles.bottom}>
             <TouchableOpacity
                 style={styles.button}
